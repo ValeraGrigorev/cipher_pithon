3f6609d9ed36c0e351ef9300f62291e2bc9afee3
ZXCSP <madinagigitova@gmail.com>
2023-12-13 19:25:22 +0700

    added main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+Simvols = (list('АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя'))
+SMS = input()
+Key = input()
+Sicrets = ""
+Key_SMS = (Key * 10)[:len(SMS)]
+for i in range(len(SMS)):
+    SMS_index_Alphavit = Simvols.index(SMS[i])
+    SMS_index_key = +  SMS_index_Alphavit Simvols.index(Key_SMS[i]))
+    SMS_index = letter_index_key
+for j in Key:
+    
+    Sicrets =+ i
